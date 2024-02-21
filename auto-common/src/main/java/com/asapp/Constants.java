2ca3d440760830415367da6987abb7fe30373cc2
unknown <anbumaran@hotmail.com>
2024-02-21 14:49:04 +0530

    fix extent report issue

--- auto-common/src/main/java/com/asapp/Constants.java
+++ auto-common/src/main/java/com/asapp/Constants.java
@@ -3,7 +3,7 @@ package com.asapp;
 public class Constants {
 
     public static final int MAX_RETRY = 2;
-    public static final String EXTENT_REPORTS_DIR = "/build/extent-reports";
+    public static final String EXTENT_REPORTS_DIR = "./build/extent-reports/";
     public static final String TASK_NAME = "testautomation.task";
     public static final String REPORT_NAME = "ASAPP - Shopping - Task '%s' - Browser '%s' - Env '%s'";
     public static final String IGNORE_STACK_TRACE = "jdk.internal";
---
+++ auto-jbasics/src/test/java/com/asapp/jbasics/MyJavaBasics.java
@@ -0,0 +1,12 @@
+package com.asapp.jbasics;
+
+import org.testng.annotations.Test;
+
+public class MyJavaBasics {
+
+    @Test
+    public void testJava() {
+        System.out.println("Praise The Lord....");
+    }
+
+}
