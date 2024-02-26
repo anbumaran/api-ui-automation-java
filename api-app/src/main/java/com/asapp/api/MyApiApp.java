3a4bb7b28e1e6fe2be5252cfaffac9e79d0bb483
unknown <anbumaran@hotmail.com>
2024-02-26 13:15:18 +0530

    fix execution issues

--- api-app/src/main/java/com/asapp/api/MyApiApp.java
+++ api-app/src/main/java/com/asapp/api/MyApiApp.java
@@ -1,5 +1,7 @@
 package com.asapp.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,8 +10,10 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.asapp.api"})
 public class MyApiApp {
 
+    private static final Logger LOGGER = LogManager.getLogger(MyApiApp.class);
     public static void main(String[] args) {
         SpringApplication.run(MyApiApp.class, args);
+        LOGGER.info("Application Start...");
     }
 
 }
